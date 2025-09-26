55be65a89eba9083a97aa48a57826e508e2860b8
David Akinola <dakinola54@gmail.com>
2025-09-26 12:20:17 +0100

    Reading and updating capsules

--- capsules/urls.py
+++ capsules/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("create/", views.create_capsule, name="create_capsule"),
+    path("<str:title>/detail", views.capsule_detail, name="capsule_detail"),
+    path("<str:title>/update", views.update_capsule, name="update_capsule"),
 ]
--- capsules/views.py
+++ capsules/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Capsule
 
+@login_required
 def index(request): 
-    return render(request, "capsules/index.html")
+    current_user = request.user
+    capsules = Capsule.objects.filter(sender=current_user)  
+    return render(request, "capsules/index.html", {"capsules": capsules}) 
 
 @login_required
 def create_capsule(request):
@@ -23,10 +26,33 @@ def create_capsule(request):
                 recipient = recipient,
                 delivery_date = delivery_date,
             )
-            return redirect("/capsule/")
+            return redirect("home")
         except Exception as e:
             print(f"Error creating capsule: {e}")
             return HttpResponse(f"Error: Could not create capsule: {e}")
     else:
         return render(request, "capsules/create.html")
     
+@login_required
+def capsule_detail(request, title):
+    capsule = get_object_or_404(Capsule, sender=request.user, capsule_title=title)
+    return render(request, "capsules/detail.html", {"capsule": capsule})
+
+@login_required
+def update_capsule(request, title):
+    capsule = get_object_or_404(Capsule, sender=request.user, capsule_title=title)
+
+    if request.method == "POST":
+        capsule.capsule_title = request.POST.get("capsule_title")
+        capsule.content = request.POST.get("capsule_content")
+        capsule.recipient = request.POST.get("recipient")
+        capsule.delivery_date = request.POST.get("delivery_date")
+
+        try:
+            capsule.save()
+            return redirect("capsule_detail", title=capsule.capsule_title)
+        except Exception as e:
+            print(f"Error updating capsule {e}")
+            return render(request, "capsules/update.html", {"capsule": capsule, "error": f"Cannot update capsule: {e}"})
+    else: 
+        return render(request, "capsules/update.html", {"capsule": capsule})
